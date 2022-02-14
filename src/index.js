57b18ef70b3f4a58e503137a344cb4f2b65f59d7
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-02-14 12:14:51 +0100

    update redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import reactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 
 reactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 )
---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    question_category: '',
+    question_difficulty: '',
+    question_type: '',
+    number_of_questions: 50,
+    score: 0
+}
+
+const reducer = (state = initialState, actions) => {
+    return state
+};
+
+export default reducer;
