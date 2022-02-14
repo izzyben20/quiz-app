cb7bcde14e85b9a4cd2d37ca3a0e676540929632
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-02-14 12:06:02 +0100

    update useAxios

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,10 +1,26 @@
 import { Box } from '@mui/system';
-import React from 'react';
+import { Typography, Button } from '@mui/material';
+import useAxios from '../hooks/useAxios';
 
 const Questions = () => {
+  let apiUrl = `/api.php?amount=10`;
+
+  const { response, loading } = useAxios({ url: apiUrl });
+  console.log(response);
+
   return (
-    <Box mt={3}>
-      Questions
+    <Box fullWidth>
+      <Typography variant='h4'>Questions 1</Typography>
+      <Typography mt={5}>This is the question?</Typography>
+      <Box mt={2}>
+        <Button variant='contained'>Answer 1</Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant='contained'>Answer 2</Button>
+      </Box>
+      <Box mt={4}>
+        Score: 6 / 10
+      </Box>
     </Box>
   );
 };
